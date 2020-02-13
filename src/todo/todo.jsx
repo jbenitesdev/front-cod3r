86b8869319f420e05b9962feed2e95c19801135d
Joao Benites <joao.alvesbenites@gmail.com>
2020-02-13 14:13:38 -0300

    Atualização do Projeto

---
+++ src/todo/todo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+
+
+
+export class Todo extends Component { 
+
+    render(){
+        return(
+            <div>
+                <h1>Todo</h1>
+            </div>
+
+        )
+    }
+
+}
+
+
